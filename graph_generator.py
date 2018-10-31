22729e4d311f518a733ae8c626477ec2a8ff0758
Joseph Haaga <josephhaaga@Josephs-MacBook-Pro.local>
2018-10-31 15:38:16 -0400

    updated for Python 3

--- graph_generator.py
+++ graph_generator.py
@@ -10,13 +10,13 @@ target_file_name=sys.argv[2];
 output = numpy.zeros((number_of_nodes,number_of_nodes),dtype=numpy.int); 
 
 
-for i in xrange(number_of_nodes):
-	for j in xrange(number_of_nodes):
+for i in range(number_of_nodes):
+	for j in range(number_of_nodes):
 		if(random.randint(0,10)>5):
 			output[i][j]=1;
 			output[j][i]=1;
 
-print output;
+# print output;
 
 numpy.savetxt(target_file_name,output,delimiter=",",fmt=('%2u'))	
 
